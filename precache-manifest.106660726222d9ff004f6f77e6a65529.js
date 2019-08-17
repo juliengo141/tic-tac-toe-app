7863265447d4de23925a015a7f454f6b9eb7ecaa
juliengo141 <juliengo141@gmail.com>
2019-08-17 00:46:57 -0400

    Updates

---
+++ precache-manifest.106660726222d9ff004f6f77e6a65529.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92f77b06c054b8bc88f932936744ddfe",
+    "url": "/tic-tac-toe-app/index.html"
+  },
+  {
+    "revision": "4800c10f099943a24f23",
+    "url": "/tic-tac-toe-app/static/css/main.4bb8f90e.chunk.css"
+  },
+  {
+    "revision": "da6ded006ad7ed6267ab",
+    "url": "/tic-tac-toe-app/static/js/2.171dafc3.chunk.js"
+  },
+  {
+    "revision": "4800c10f099943a24f23",
+    "url": "/tic-tac-toe-app/static/js/main.1062579c.chunk.js"
+  },
+  {
+    "revision": "e5f0c2561e9d0032cbb8",
+    "url": "/tic-tac-toe-app/static/js/runtime~main.334dfbc6.js"
+  }
+]);
